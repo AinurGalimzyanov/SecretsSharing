5412b8a4a0a770675377585a3dd57237083a427c
Galimzyanov Ainur <a.galimz@mail.ru>
2023-06-01 19:47:10 +0500

    доработки

--- Logic/Managers/Files/FilesManager.cs
+++ Logic/Managers/Files/FilesManager.cs
@@ -9,6 +9,7 @@ using Logic.Managers.Base;
 using Logic.Managers.Files.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using YandexDisk.Client.Clients;
 using YandexDisk.Client.Http;
 using YandexDisk.Client.Protocol;
@@ -22,13 +23,15 @@ public class FilesManager  : BaseManager<FilesDal, Guid>, IFilesManager
 {
     private readonly UserManager<UserDal> _userManager;
     private readonly IFilesRepository _filesRepository;
+    private readonly IConfiguration _configuration;
 
 
-    public FilesManager(IFilesRepository repository, UserManager<UserDal> userManager)
+    public FilesManager(IFilesRepository repository, UserManager<UserDal> userManager, IConfiguration configuration)
         : base(repository)
     {
         _userManager = userManager;
         _filesRepository = repository;
+        _configuration = configuration;
     }
 
     /// <summary>
@@ -54,7 +57,7 @@ public class FilesManager  : BaseManager<FilesDal, Guid>, IFilesManager
     /// <param name="type">type file</param>
     private async Task UploadToCloudAsync(string userId, string dalId, IFormFile file, string type)
     {
-        var api = new DiskHttpApi("y0_AgAAAABR93eBAAn3FAAAAADj_iQ-f3UvbKf6QkOvsUWylH2gEL66jvU");
+        var api = new DiskHttpApi(_configuration["YandexDisk:oauthKey"]);
         var pathDirUser = "SecretsSharing" + "/" + userId;
         var roodFolderDate = await api.MetaInfo.GetInfoAsync(new ResourceRequest
         {
@@ -118,7 +121,7 @@ public class FilesManager  : BaseManager<FilesDal, Guid>, IFilesManager
     {
         var user = await _filesRepository.GetUserFile(dal.Id);
         var type = dal.Name.Split(".")[1];
-        var api = new DiskHttpApi("y0_AgAAAABR93eBAAn3FAAAAADj_iQ-f3UvbKf6QkOvsUWylH2gEL66jvU");
+        var api = new DiskHttpApi(_configuration["YandexDisk:oauthKey"]);
         var path = $"/SecretsSharing/{user.Id}/{dal.Id}.{type}";
         var fileStream = await api.Files.DownloadFileAsync(path);
         var fileType="application/octet-stream";
@@ -139,7 +142,7 @@ public class FilesManager  : BaseManager<FilesDal, Guid>, IFilesManager
     {
         var files = new List<FilesDal>();
         var user = await FindUser(token);
-        var api = new DiskHttpApi("y0_AgAAAABR93eBAAn3FAAAAADj_iQ-f3UvbKf6QkOvsUWylH2gEL66jvU");
+        var api = new DiskHttpApi(_configuration["YandexDisk:oauthKey"]);
         var roodFolderDate = await api.MetaInfo.GetInfoAsync(new ResourceRequest
         {
             Path = $"/SecretsSharing/{user.Id}"
@@ -164,7 +167,7 @@ public class FilesManager  : BaseManager<FilesDal, Guid>, IFilesManager
         var file = await GetAsync(fileId);
         var user = await _filesRepository.GetUserFile(file.Id);
         var type = file.Name.Split(".")[1];
-        var api = new DiskHttpApi("y0_AgAAAABR93eBAAn3FAAAAADj_iQ-f3UvbKf6QkOvsUWylH2gEL66jvU");
+        var api = new DiskHttpApi(_configuration["YandexDisk:oauthKey"]);
         var path = $"/SecretsSharing/{user.Id}/{file.Id}.{type}";
         await api.Commands.DeleteAsync(new DeleteFileRequest() {Path = path});
         await DeleteAsync(fileId);
